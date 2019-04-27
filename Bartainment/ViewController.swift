5a9b9fb6390010c0c81575a46647eb134f01c71c
zacharyesm <zackesmaelzada@gmail.com>
2019-04-27 10:56:58 -0400

    project init

---
+++ Bartainment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bartainment
+//
+//  Created by Zack Esm on 4/27/19.
+//  Copyright © 2019 HackFest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
