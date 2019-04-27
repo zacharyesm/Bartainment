f4a54bc75f1b9f71e8719523d3caefb6f2ad3611
zacharyesm <zackesmaelzada@gmail.com>
2019-04-27 11:48:07 -0400

    added basic tableview template

--- Bartainment/Controller/BarViewController.swift
+++ Bartainment/Controller/BarViewController.swift
@@ -9,11 +9,59 @@
 import UIKit
 
 class BarViewController: UIViewController {
+    
+    lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.delegate = self
+        tv.dataSource = self
+        return tv
+    }()
+    
+    let seekingJobs = ["Singer", "Live Band", "Comedian", "DJ"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .blue
+        
+        setupView()
+    }
+    
+    fileprivate func setupView() {
+        let margins = view.safeAreaLayoutGuide
+        
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: margins.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: margins.bottomAnchor)
+            ])
     }
 
+}
+
+extension BarViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return seekingJobs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = seekingJobs[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(seekingJobs[indexPath.row])
+//        let vc = EntertainerViewController()
+//        navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
